caab005b0d390651205d7d50ca09e3cdfdc58fd4
Miguel <miguel@ganesha>
2020-12-24 18:56:45 -0300

    updated SQL query on cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -6,4 +6,4 @@ port=$( cat database.ts | grep -oP "(?<= PORT_DB: ).*(?=,)" ) \
 dbname=$( cat database.ts | grep -oP "(?<= DB: ').*(?=',)" ) \
 user=$( cat database.ts | grep -oP "(?<= USER_DB: ').*(?=',)" ) \
 password=$( cat database.ts | grep -oP "(?<= PASSWORD: ').*(?=',)" )\
-" -c 'CALL run()'
+" -c 'CALL run();'
